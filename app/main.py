59203c21d447b9d2c9c23c751dd37ccd24919932
IndarKarhana <79403190+IndarKarhana@users.noreply.github.com>
2024-02-26 23:41:12 -0800

    Update main.py
    added logo

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ def main():
             type=["pdf"],
         )
 
-        if uploaded_file and OPENAI_KEY is not None:
+        if uploaded_file and OPENAI_KEY:
             with st.spinner("Processing..."):
                 # NamedTemporaryFile to handle the uploaded file
                 with NamedTemporaryFile(delete=False) as temp_file:
@@ -49,76 +49,67 @@ def main():
 
                     loader = PyPDFLoader(temp_file.name)
                     docs = loader.load()
-                ### embeddings model
-                MODEL_NAME = "text-embedding-3-small"
+                
+                # Attempt to create an instance of ChatOpenAI to verify the key
+                try:
+                    llm_model = ChatOpenAI(
+                        model="gpt-4-turbo-preview",
+                        temperature=0,
+                        openai_api_key=OPENAI_KEY,
+                    )
+                except Exception as e:
+                    st.error(f"Invalid API Key: {e}")
+                    return
+                
                 embeddings = OpenAIEmbeddings(
-                    model=MODEL_NAME,
+                    model="text-embedding-3-small",
                     api_key=OPENAI_KEY,
-                )
-
-                llm_model = ChatOpenAI(
-                    # model="gpt-3.5-turbo",
-                    model="gpt-4-turbo-preview",
-                    temperature=0,
-                    openai_api_key=OPENAI_KEY,
                 )
                 vectordb = FAISS.from_documents(
                     documents=docs,
                     embedding=embeddings,
                 )
-            st.success("Processing finished.")
-
-            ## RAG Chain with out source documents in output
-            # rag_chain = (
-            #     {
-            #         "context": vectordb.as_retriever(
-            #             search_kwargs={"k": 3, "search_type": "similarity"}
-            #         ),
-            #         "question": RunnablePassthrough(),
-            #     }
-            #     | prompt
-            #     | llm_model
-            #     | StrOutputParser()
-            # )
-
-            ## RAG Chain from docs =
-            rag_chain_from_docs = (
-                RunnablePassthrough.assign(
-                    context=(lambda x: format_docs(x["context"]))
+                st.success("Processing finished.")
+                
+                # RAG Chain setup
+                rag_chain_from_docs = (
+                    RunnablePassthrough.assign(
+                        context=(lambda x: format_docs(x["context"]))
+                    )
+                    | prompt
+                    | llm_model
+                    | StrOutputParser()
                 )
-                | prompt
-                | llm_model
-                | StrOutputParser()
-            )
 
-            rag_chain_with_source = RunnableParallel(
-                {
-                    "context": vectordb.as_retriever(
-                        search_kwargs={"k": 3, "search_type": "similarity"}
-                    ),
-                    "question": RunnablePassthrough(),
-                }
-            ).assign(answer=rag_chain_from_docs)
+                rag_chain_with_source = RunnableParallel(
+                    {
+                        "context": vectordb.as_retriever(
+                            search_kwargs={"k": 3, "search_type": "similarity"}
+                        ),
+                        "question": RunnablePassthrough(),
+                    }
+                ).assign(answer=rag_chain_from_docs)
 
-            with col1:
-                st.header("Query")
-                input_text = st.text_area("Enter Text", height=300)
-                if st.button("Submit"):
-                    with st.spinner("Processing..."):
-                        response = rag_chain_with_source.invoke(input_text)
-                        output_text = response["answer"]
+                with col1:
+                    st.header("Query")
+                    input_text = st.text_area("Enter Text", height=300)
+                    if st.button("Submit"):
+                        with st.spinner("Processing..."):
+                            response = rag_chain_with_source.invoke(input_text)
+                            output_text = response["answer"]
 
-                        output_text += (
-                            f" Following pages from {uploaded_file.name} has additional information for reference: "
-                            + str([doc.metadata["page"] for doc in response["context"]])
-                        )
-                        st.header("Output")
-                        st.write(output_text)
-        else:
-            st.error("Invalid api key.")
+                            output_text += (
+                                f" Following pages from {uploaded_file.name} has additional information for reference: "
+                                + str([doc.metadata["page"] for doc in response["context"]])
+                            )
+                            st.header("Output")
+                            st.write(output_text)
+        elif not OPENAI_KEY:
+            st.error("Please enter an API key.")
+    st.sidebar.image("../imgs/booksmadeeasy.png", caption='', use_column_width=True)      
     st.sidebar.markdown("### Disclaimer")
     st.sidebar.write(
-        "Attention! Tier 1 OpenAI key is expected. The key is not saved or stored anywhere. \
+        "Tier 1 OpenAI key is expected. The key is not saved or stored anywhere. \
             It is only used for authentication purposes during the session"
     )
 
